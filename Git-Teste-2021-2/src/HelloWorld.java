785e1edf1eedc323082b496814c18b0d62769276
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2021-11-18 09:46:34 -0300

    Commit SSH

--- Git-Teste-2021-2/src/HelloWorld.java
+++ Git-Teste-2021-2/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!!");
 		System.out.println("Copia Eclipse");
-		System.out.println("Copia BASH - Novo Commit");
+		System.out.println("Copia BASH - Novo Commit - SSH");
 	}
 
 }
