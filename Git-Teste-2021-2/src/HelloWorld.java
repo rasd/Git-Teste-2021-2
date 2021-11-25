c74baf1cf523944bc2cfbac038bce1d8c89d852b
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2021-11-25 12:04:04 -0200

    Testando reimport.

--- Git-Teste-2021-2/src/HelloWorld.java
+++ Git-Teste-2021-2/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!!!!!");
 		System.out.println("Copia Eclipse");
 		System.out.println("Copia BASH - Novo Commit - SSH");
+		System.out.println("Testando Import!");
 	}
 
 }
